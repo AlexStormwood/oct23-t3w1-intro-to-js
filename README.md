cce58b02d1ef785dd62345c388cca4975d54d257
Alex H <alex.holder@bigfootds.com>
2024-04-06 11:54:23 +1100

    feat: json parse and stringify example

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # oct23-t3w1-intro-to-js
+
+1. JavaScript
+2. NodeJS
+3. ReactJS
+4. ExpressJS
+5. MongoDB
--- exampleOtherFile.js
+++ exampleOtherFile.js
@@ -1,4 +1,39 @@
+// import syntax works for React, needs different tweaks for vanilla JS
+// import {logTheTeam as originalLogger}  from "./script";
+
+
+
+logTheTeam();
+// originalLogger();
 
 
 console.log(person.name);
+
+// import data from "./jsonExample.json";
+
+// let data = fetch("./jsonExample.json").then(data => {return data});
+
+// console.log(data);
+
+let exampleJson = {
+	"name":"Alex",
+	"favouriteFood":[
+		"pizza",
+		"pies",
+		"mangoes"
+	]
+}
+
+let jsonString = JSON.stringify(exampleJson);
+
+console.log(JSON.stringify(exampleJson));
+
+// Load your JSON 
+let processedJson = JSON.parse(jsonString);
+
+console.log(processedJson);
+processedJson.name = "Batman";
+
+console.log(JSON.stringify(processedJson));
+
 
