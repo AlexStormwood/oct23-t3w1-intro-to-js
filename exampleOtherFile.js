bf6743ba570fc692a4fa32ca1e4389fdb292adcf
Alex H <alex.holder@bigfootds.com>
2024-04-06 11:57:52 +1100

    docs: add JSON process

--- exampleOtherFile.js
+++ exampleOtherFile.js
@@ -24,6 +24,15 @@ let exampleJson = {
 	]
 }
 
+
+/*
+
+1. JSON.parse to convert a JSON string into an object
+2. Modify the object however you want
+3. Save the JSON as a JSON string by stringifying it
+
+*/
+
 let jsonString = JSON.stringify(exampleJson);
 
 console.log(JSON.stringify(exampleJson));
